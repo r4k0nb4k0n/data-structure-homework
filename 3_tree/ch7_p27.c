a2135920df32271391b0c42a675eceeb5fd668f4
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-07 22:49:20 +0900

    Update ch7_p27.c

--- 3_tree/ch7_p27.c
+++ 3_tree/ch7_p27.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 #define SIZE 10
 typedef int element;
-typedef struct TreeNode {
+typedef struct Queue { // 배열 큐
+	TreeNode *data;
+	int front, rear;
+	int maxsize;
+	int size;
+} Queue;
+typedef struct TreeNode { // 트리 노드
 	element data;
 	struct TreeNode *left, *right;
 } TreeNode;
-TreeNode *n[SIZE];
+TreeNode *n[SIZE]; // 트리 노드들의 주소들을 저장할 포인터 배열
 
-TreeNode *init(void){
+void queue_init(Queue *q, int sz); int queue_size(Queue *q); int queue_front(Queue *q);
+int queue_empty(Queue *q); int queue_full(Queue *q);
+void queue_enqueue(Queue *q, element val); void queue_dequeue(Queue *q);
+TreeNode *tree_init(void){ // 과제에 주어진 트리대로 초기화 후 해당 트리의 루트 노드 주소 리턴
 	for(i=0;i<SIZE;i++) n[i] = (TreeNode *)malloc(sizeof(TreeNode));
 	*(n[0]) = { 8, n[1], n[2] };
 	*(n[1]) = { 5, n[3], n[4] };
@@ -22,13 +31,27 @@ TreeNode *init(void){
 	*(n[9]) = { 2, NULL, NULL };
 	return n[0]; // 트리의 루트 노드 주소 리턴
 }
-TreeNode max(TreeNode *root);
-TreeNode min(TreeNode *root);
-TreeNode level_traversal(TreeNode *root);
+TreeNode *max(TreeNode *root);
+TreeNode *min(TreeNode *root);
+void level_traversal(TreeNode *root);
 
 int main(void){
-	TreeNode *root = init(); // 트리 초기화
-	int i;
+	TreeNode *root = tree_init(); // 트리 초기화
+	printf("Max element of the tree is %d\n", max(root)->data);
+	printf("Min element of the tree is %d\n", min(root)->data);
+	printf("Level Traversal of the tree => "); level_traversal(root);
+	return 0;
+}
+
+TreeNode *max(TreeNode *root){
+	
+}
+
+TreeNode *min(TreeNode *root){
 
 }
 
+void level_traversal(TreeNode *root){
+	Queue q; queue_init(&q, SIZE);
+	queue_push(&q, 
+}
